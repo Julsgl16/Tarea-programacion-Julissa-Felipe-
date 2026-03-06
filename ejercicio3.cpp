37a85f905a43e99fbca212a989c8dbe848b60ae6
Julsgl16 <hfelipeg@miumg.edu.gt>
2026-03-05 19:00:03 -0600

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+int cantidad;
+cout << "Cuantos numeros vas a ingresar? ";
+cin >> cantidad;
+int numeros[cantidad];
+int inversos[cantidad];
+for (int i = 0; i < cantidad; i++) {
+    cout << "Numero " << i + 1 << ": ";
+    cin >> numeros[i];
+}
+for (int i = 0; i < cantidad; i++) {
+    inversos[i] = numeros[i] * -1;
+}
