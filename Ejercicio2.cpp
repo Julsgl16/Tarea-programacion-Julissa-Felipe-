b9c152a5a536878f53302eb4137c409088371eb3
Julsgl16 <hfelipeg@miumg.edu.gt>
2026-03-05 19:00:35 -0600

    Update Ejercicio2.cpp

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
 int n;
 cout << "Ingresa un numero entero positivo: ";
 cin >> n;
@@ -11,3 +15,4 @@ for (int i = 0; i < n; i++) {
     if (i < n - 1) cout << ", ";
 }
 cout << "]" << endl;
+}
