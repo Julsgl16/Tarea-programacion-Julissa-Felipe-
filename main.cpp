56b9aebea6d6ff422b8d0ef6fdd3a0b6ebdd8203
Julsgl16 <hfelipeg@miumg.edu.gt>
2026-03-05 18:42:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hola Julissa y Felipe!" << endl;
+    string cadena;
+    long long decimal =0; 
+    cout<< "ingrese un numero binario 1011 ";
+        cin>> cadena;
+    for (char c : cadena){
+        if(c !='0' && c !='1'){
+            cout << "Error, la cadena no es binaria\n";
+            return 0; 
+        }
+        decimal = decimal *2+(c-'0');
+    }
+    cout<< "decimal: "<< decimal << "\n";
     return 0;
 }
