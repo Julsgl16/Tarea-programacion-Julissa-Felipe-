899ec92216dfec75aefbbc9eed5431fd1b717a6b
Julsgl16 <hfelipeg@miumg.edu.gt>
2026-03-05 19:55:20 -0600

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -2,31 +2,47 @@
 #include <string>
 using namespace std;
 
-int main()
-{
+int main() {
     string cadena;
     int pos1, pos2;
-    cout << "ingrese una cadena sin espacios";
+
+    cout << "Ingrese una cadena sin espacios: ";
     cin >> cadena;
-    cout << "ingrese posicion 1 ";
-    cin >> pos1;
-    cout << "ingrese posicion 2 ";
-    cin >> pos2;
 
     if (cadena.empty()) {
-        cout << "cadena vacia\n";
+        cout << "Cadena vacia\n";
         return 0;
     }
+
+    cout << "Ingrese posicion 1: ";
+    cin >> pos1;
+    cout << "Ingrese posicion 2: ";
+    cin >> pos2;
     if (pos1 < 0) pos1 = 0;
     if (pos2 >= (int)cadena.size()) pos2 = (int)cadena.size() - 1;
-    while (pos1 < pos2) {
+    if (pos1 >= pos2) {
+        cout << "Las posiciones no son validas para invertir\n";
+        return 0;
+    }
 
+    while (pos1 < pos2) {
         char aux = cadena[pos1];
         cadena[pos1] = cadena[pos2];
         cadena[pos2] = aux;
-
         pos1++;
         pos2--;
     }
+    cout << "Cadena resultante: " << cadena << endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+    
+}
+    return 0;
